0f4318b56fda35a27713b62eb9f153b1dcf36204
ct5w9 <ct5w9@mst.edu>
2017-04-24 00:31:41 -0500

    Added Login Window

--- controller.py
+++ controller.py
@@ -2,9 +2,8 @@ import model as Model
 import view as View
 import threading
 from view import ChatRoom
+from view import Login
 import queue
-
-
 
 def listen(ws, q):
 	try:
@@ -19,56 +18,71 @@ def listen(ws, q):
 
 if __name__ == '__main__':
 
-		ws = Model.createConnection()
+	ws = Model.createConnection()
 
-		recvQueue = queue.Queue()
+	recvQueue = queue.Queue()
 
-		listener = threading.Thread(target=listen, args=(ws,recvQueue,))
-		listener.setDaemon(True)
+	listener = threading.Thread(target=listen, args=(ws,recvQueue,))
+	listener.setDaemon(True)
 
-		listener.start()
+	listener.start()
 
-		username = Model.consoleInput("Username: ")[1]
-		#password = consoleInput("Password: ")[1]
-		Model.login(ws, username)
+	L = Login()
 
-		C = ChatRoom()
-		C.display_message("Hello World! Welcome to the chatroom!", "CHATROOM")
-
-		# TODO: Login authentication
+	LoginWindow = True
+	# set validLog to true if valid username & pass, otherwise false
+	validLog = True
+	#Main Login Loop
+	while LoginWindow:
+		L.update()
+		#TODO Login Authentication
 		'''
 		try:
 			response = recvQueue.get_nowait()
 			if response['action'] == 'loginFailure':
 				Model.printToConsole(response)
+				validLog = False
 		except queue.Empty:
 			# Nothing received from server
 		'''
+		if (L.login and not validLog):
+			L.password.configure(text="Password: (Does not match)", fg="red")
+			L.login = 0
 
-		# Main loop
-		# TODO: Make this loop an event loop
-		while True:
-			# Read user input
-			#run, buff = Model.consoleInput() # Implement event polling so that this line no longer blocks
-			C.update()
-			if C.TB:
-				jsonData = Model.pythonToJson('message', 'text', C.T)
-				Model.send(ws, jsonData)
-				C.TB = 0
+		if validLog and L.login:
+			L.login = 0
+			LoginWindow = False
+			Model.login(ws, L.u)
+			L.destroy()
+
+	C = ChatRoom()
+	C.display_message("Hello World! Welcome to the chatroom!", "CHATROOM")
 
 
-			# Exit loop if exit command was sent
-			#if not run:
-			#	break
+	# Main loop
+	# TODO: Make this loop an event loop
+	while True:
+		# Read user input
+		#run, buff = Model.consoleInput() # Implement event polling so that this line no longer blocks
 
-			
-			# Send data to View
-			try:
-				data = recvQueue.get_nowait()
-				if data['action'] == 'userMessage':
-					C.display_message(data['text'], data['user'])
-			except queue.Empty:
-				pass
-			
+		C.update()
 
-		Model.closeConnection(ws)
+		if C.TB:
+			jsonData = Model.pythonToJson('message', 'text', C.T)
+			Model.send(ws, jsonData)
+			C.TB = 0
+
+		# Exit loop if exit command was sent
+		#if not run:
+		#	break
+
+		# Send data to View
+		try:
+			data = recvQueue.get_nowait()
+			if data['action'] == 'userMessage':
+				C.display_message(data['text'], data['user'])
+		except queue.Empty:
+			pass
+
+
+	Model.closeConnection(ws)
--- view.py
+++ view.py
@@ -80,10 +80,142 @@ class ChatRoom(tk.Tk):
         self.TB = 1
         self.entry2.delete(0, END)
 
-        
 
-        
-    
+"""
+Login Window
+"""
+class Login(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        def setColor(event):
+            if (self.focus_get() == userInput):
+                line.configure(bg="black")
+            if (self.focus_get() == passInput):
+                line2.configure(bg="black")
+
+        def resetColor(event):
+            if (self.focus_get() != userInput):
+                line.configure(bg="grey")
+            if (self.focus_get() != passInput):
+                line2.configure(bg="grey")
+
+        def setLine(event):
+            line3.configure(bg="black")
+
+        def resetLine(event):
+            line3.configure(bg="#F0F0F0")
+
+        def setSubmitColor(event):
+            submit.configure(bg="#B31E1E")
+
+        def resetSubmitColor(event):
+            submit.configure(bg="#e62727")
+
+        def Register_toggle(event):
+            # switching to registration
+            if (welcome.cget("text") == "Please Log In"):
+                welcome.configure(text="Please Register")
+                submit.configure(text="Register", padx=140)
+                needAcc.configure(text="Have an account?")
+                register.configure(text="Log In")
+                line3.configure(width=35)
+                self.password.configure(text="Password:", fg="black")
+            else:
+                welcome.configure(text="Please Log In")
+                submit.configure(text="Login", padx=150)
+                needAcc.configure(text="Need an account?")
+                register.configure(text="Register")
+                line3.configure(width=46)
+                self.password.configure(text="Password:", fg="black")
+
+        def on_click():
+            self.login = 1
+            self.u = userInput.get()
+            self.p = passInput.get()
+
+        #Main Window
+        self.title("Login")
+        self.login = 0
+        self.u = ""
+        self.p = ""
+
+        w = 400  # width for the Tk self
+        h = 425  # height for the Tk self
+
+        # get screen width and height
+        ws = self.winfo_screenwidth()  # width of the screen
+        hs = self.winfo_screenheight()  # height of the screen
+
+        #centering the window
+        x = (ws / 2) - (w / 2)
+        y = (hs / 2) - (h / 2)
+
+        # set the dimensions of the screen
+        # and where it is placed
+        self.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+        # Welcome Label
+        welcome = tk.Label(self, text="Please Log In", font="Sans/Serif 15 bold")
+        welcome.place(x=130, y=20)
+
+        # Username Label
+        username = tk.Label(self, text="Username:", font="Sans/Serif 9")
+        username.place(x=30, y=70)
+
+        # Password Label
+        self.password = tk.Label(self, text="Password:", font="Sans/Serif 9")
+        self.password.place(x=30, y=153)
+
+        # NeedanAccount Label
+        needAcc = tk.Label(self, text="Need an account?", font="Sans/Serif 9")
+        needAcc.place(x=30, y=380)
+
+        # Register/Button
+        register = tk.Label(self, text="Register", font="Sans/Serif 9")
+        register.place(x=135, y=380)
+
+        register.bind("<Enter>", setLine)
+        register.bind("<Leave>", resetLine)
+        register.bind("<Button-1>", Register_toggle)
+
+        # Entries
+        color = '#F0F0F0'
+        userInput = tk.Entry(self, bg=color, borderwidth=0, width=58)
+        passInput = tk.Entry(self, show="*", bg=color, borderwidth=0, width=58)
+
+        userInput.place(x=35, y=113)
+        passInput.place(x=35, y=198)
+
+        # Lines
+        canvas_width = 344
+        canvas_height = 2
+        # line = under username, line2 = under password, line3 = under register
+
+        line = tk.Canvas(self, width=canvas_width, height=canvas_height, bg="grey")
+        line2 = tk.Canvas(self, width=canvas_width, height=canvas_height, bg="grey")
+        line3 = tk.Canvas(self, width=46, height=1, bg="black")
+
+        line.place(x=30, y=135)
+        line2.place(x=30, y=220)
+        line3.place(x=136, y=397)
+        line3.configure(bg="#F0F0F0")
+
+        userInput.bind("<FocusIn>", setColor)
+        userInput.bind("<FocusOut>", resetColor)
+        passInput.bind("<FocusIn>", setColor)
+        passInput.bind("<FocusOut>", resetColor)
+
+        #Submit Button
+        submit = tk.Button(self, text='Login', padx=150, bg="#e62727", activebackground="#B31E1E",
+                           activeforeground="white", fg="white", font="Sans/serif 12 bold", pady=15,
+                           command=on_click)
+        submit.bind("<Enter>", setSubmitColor)
+        submit.bind("<Leave>", resetSubmitColor)
+
+        submit.place(x=30, y=320)
+
+
 #Temporary function for printing
 def printToConsole(data):
     if data['action'] == 'userMessage':
